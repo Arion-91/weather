3dff0d38f914ff6c10c72f903e391523bcdc681e
Arion <arion2107@gmail.com>
2019-07-25 15:57:39 +0600

    Свертана середина

--- src/components/Middle.vue
+++ src/components/Middle.vue
@@ -1,5 +1,11 @@
 <template>
-
+	<div class="middle">
+		<div class="degree">
+			<img src="../assets/cloud.png" alt="cloud">
+			<p>19&deg;</p>
+		</div>
+		<p>Преимущественно солнечно</p>
+	</div>
 </template>
 
 <script>
@@ -9,5 +15,22 @@
 </script>
 
 <style scoped>
+	.middle {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
 
+	.degree {
+		display: flex;
+		margin: 20px;
+	}
+
+	.degree p {
+		font-size: 100px;
+	}
+
+	p {
+		font-size: 25px;
+	}
 </style>
